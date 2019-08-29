c19e917b8550ed5b5ebcf5a3a401bbd47cf76cf4
Vinita Gupta <vinita.gupta@publicissapient.com>
2019-08-29 14:45:41 +0530

    added server code

--- single-spa.config.js
+++ single-spa.config.js
@@ -13,12 +13,3 @@ registerApplication(
 );
 
 start();
-const express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
-const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port,function(){
-    console.log("app is running");
-});
